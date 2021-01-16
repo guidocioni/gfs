5a2f336f4dafe340bdefb41ac230816b65784390
Guido Cioni <guidocioni@gmail.com>
2021-01-16 17:25:16 +0100

    First commit

---
+++ functions_download_gefs.sh
@@ -0,0 +1,13 @@
+download_gfs_data()
+{
+# Make sure the forecast hour is 2+ digits
+fcstHour=$(printf "%03d" $1)
+
+url_control="https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${YEAR}${MONTH}${DATE}/${RUN}/gfs.t${RUN}z.pgrb2.0p25.f${fcstHour}"
+echo $url_control
+timeout 300 perl $HOME_SCRIPTS/get_inv.pl "${url_control}.idx" | \
+        grep -E ":((TMP|DPT|TMAX|TMIN):2 m above ground|(TMP|HGT|RH):(5|10|20|50|100|200|250|300|400|500|600|700|850|900|925|950|975|1000) mb|(UGRD|VGRD):10 m above ground|(CAPE|CIN|GUST):surface|PRMSL|APCP:surface:0-(*.)|TCDC:entire atmosphere|SNOD)" | \
+        timeout 300 perl $HOME_SCRIPTS/get_grib.pl "${url_control}" $GRIBDIR/grib_gfs_"$YEAR""$MONTH""$DATE"_"$RUN"_"$fcstHour".grib
+
+}
+export -f download_gfs_data
